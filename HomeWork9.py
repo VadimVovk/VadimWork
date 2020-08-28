2cef4ec558b57a195b61fd21efca3fbfb6a16fbd
vadim <vadimvovk77@gmail.com>
2020-08-28 17:59:26 +0300

    Обновление 20.08

--- HomeWork9.py
+++ HomeWork9.py
@@ -1,42 +1,42 @@
 import string
 import random
-def create_string(txt):
+def create_string(str_random):
     string_symbol = string.printable
-my_list = []
-my_list_num = []
-lrn = random.randint(100, 150)
-for num in range(lrn):
-    my_list.append(random.choice(string_symbol))
-    my_list_num.append(num)
-my_list_num_f = my_list_num[1:lrn - 1]
-random.shuffle(my_list_num_f)
-list_9 = my_list_num_f[0:9]
-for symbol in list_9:
-    my_list[symbol] = "\n"
-print("".join(my_list))
-
+    my_list = []
+    my_list_num = []
+    lrn = random.randint(100,1000)
+    for num in range(lrn):
+        my_list.append(random.choice(string_symbol))
+        my_list_num.append(num)
+    my_list_num_f = my_list_num[1:lrn - 1]
+    random.shuffle(my_list_num_f)
+    list_9 = my_list_num_f[0:9]
+    for symbol in list_9:
+        my_list[symbol] = "\n"
+    str_random=str("".join(my_list))
+    print(str_random)
 ##################
-string_symbol = string.ascii_lowercase
-key_list=[]
-my_dict={}
-quantity_key=random.randint(5,20)
-for i in range(quantity_key):
-    key_list.append("".join(random.choices(string.ascii_lowercase,k=5)))
-# for num in range(quantity_key):
-    x=random.randint(1,3)
-    if x == 1:
-        data = random.randint(-100,100)
-    elif x == 2:
-        data = random.uniform(0,1)
-    else:
-        y=random.randint(1,2)
-        if y==1:
-            data = True
-        else:
-            data = False
-    my_dict.update({key_list[i]:data})
-print(my_dict)
-########
+# string_symbol = string.ascii_lowercase
+# key_list=[]
+# my_dict={}
+# quantity_key=random.randint(5,20)
+# for i in range(quantity_key):
+#     key_list.append("".join(random.choices(string.ascii_lowercase,k=5)))
+# # for num in range(quantity_key):
+#     x=random.randint(1,3)
+#     if x == 1:
+#         data = random.randint(-100,100)
+#     elif x == 2:
+#         data = random.random()
+#     else:
+#         y=random.randint(1,2)
+#         if y==1:
+#             data = True
+#         else:
+#             data = False
+#     my_dict.update({key_list[i]:data})
+# print(my_dict)
+# ########
 # my_list=[]
 # my_list_f=[]
 # n=random.randint(3,10)
@@ -48,10 +48,10 @@ print(my_dict)
 # print(len(my_list[1]))
 ##########
 def file_writer(my_path):
-    file_name = my_path.rsplit(".",1)
-    if file_name ="txt":
+    file_resolution = my_path.rsplit(".",1)
+    if file_resolution[-1] == "txt":
+        with open("my_path", "w") as txtfile:
+            txtfile.write(create_string(str))
 
-
-
-
-my_str="F:/Vadim/PycharmProjects/First/first.py"
+my_str="F:/Vadim/PycharmProjects/First/first.txt"
+file_writer(my_str)
--- Second.py
+++ Second.py
@@ -1,46 +1,26 @@
-###########
-# s = "43 больше 38, но меньше 56 "
-# l = len(s)
-# integ = []
-# i = 0
-# while i < l:
-#     s_int = ''
-#     a = s[i]
-#     while '0' <= a <= '9':
-#         s_int += a
-#         i += 1
-#         if i < l:
-#             a = s[i]
-#         else:
-#             break
-#     i += 1
-#     if s_int != '':
-#         integ.append(int(s_int))
-# print(integ)
-#########
-# str = "012345678"
-# len_s = len(str)
-# if len_s % 2 == 0:
-#     my_list = list(str)
-# else:
-#     my_list= list(str)
-#     # my_list.appehd(" ")
-# # my_list =([str[i:i + 2] for i in range(0, len_s, 2)])
-# # my_list.append(" ")
-# print(my_list)
+import string
 import random
-def r_coord(tuple_1)
-    a = -100
-    b = 100
-    Tuple_1=(random.randint(a,b),random.randint(a,b))
+def create_string(str_random):
+    string_symbol = string.printable
+    my_list = []
+    my_list_num = []
+    lrn = random.randint(100,1000)
+    for num in range(lrn):
+        my_list.append(random.choice(string_symbol))
+        my_list_num.append(num)
+    my_list_num_f = my_list_num[1:lrn - 1]
+    random.shuffle(my_list_num_f)
+    list_9 = my_list_num_f[0:9]
+    for symbol in list_9:
+        my_list[symbol] = "\n"
+    str_random=str("".join(my_list))
+    print(str_random)
 
-# a_x = random.randint(a,b)
-# a_y = random.randint(a,b)
-# b_x = random.randint(a,b)
-# b_y = random.randint(a,b)
-# c_x = random.randint(a,b)
-# c_y = random.randint(a,b)
-triangle = {"A" : ),
-            "B":(b_x,b_y),
-            "C":(c_x,c_y)}
-print(triangle["A"])
+# file_writer(my_path):
+my_str = "F:/Vadim/PycharmProjects/First/first.txt"
+file_resolution = my_str.rsplit(".",1)
+if file_resolution[-1] == "txt":
+    create_string(str_1)
+    with open("my_path", "w") as file:
+    file.write("str_1")
+    print(str_f)
