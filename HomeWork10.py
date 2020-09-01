b9ea47c397f69d58372d5d01f490f4e928aada52
vadim <vadimvovk77@gmail.com>
2020-09-02 00:26:16 +0300

    Обновление 20.08

--- HomeWork10.py
+++ HomeWork10.py
@@ -5,21 +5,24 @@ def read_json(filename_path):
         data = json.load(json_file)
     return data
 my_str="F:/Vadim/PycharmProjects/First/data.json"
-# # print(read_json(my_str))
 
 # def key_sort(maths_dict):
 #     name=maths_dict.get("name")
 #     name=re.findall(r'\w+$', name)
 #     return name
 #
-data_dict=read_json(my_str)
+# data=read_json(my_str)
 # name_sorted=sorted(data,key=key_sort)
 # [print(line) for line in name_sorted]
 
 def key_sort_date(maths_dict):
-    years=maths_dict.get("years")
-    years=re.findall(r'\d+$', years )
-    return years
-years_sorted=sorted(data_dict,key=key_sort_date)
+    years = maths_dict.get("years")
+    years_str_num = re.findall(r'\d+', years)
+    years_num=[int(number) for number in years_str_num]
+    d_date= max(years_num) if years.count("BC") < 2 else -1*min(years_num)
+    return d_date
+data=read_json(my_str)
+
+years_sorted=sorted(data, key=key_sort_date)
 [print(line) for line in years_sorted]
 
