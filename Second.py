b9213fdcbe1049ae5157b23fdb01ae8fee7ae132
vadim <vadimvovk77@gmail.com>
2020-09-08 16:27:26 +0300

    Обновление 08.09

--- Second.py
+++ Second.py
@@ -1,24 +1,27 @@
-import string
-import random
-import json
-import csv
-# create_dict_json():
-key_list=[]
-my_dict={}
-quantity_key=random.randint(5,20)
-for i in range(quantity_key):
-    key_list.append("".join(random.choices(string.ascii_lowercase,k=5)))
-# for num in range(quantity_key):
-    x=random.randint(1,3)
-    if x == 1:
-        dat = random.randint(-100,100)
-    elif x == 2:
-        dat = random.random()
-    else:
-        y=random.randint(1,2)
-        if y==1:
-            dat = True
-        else:
-            dat = False
-    my_dict.update({key_list[i]:dat})
-print(my_dict)
+import os
+def listdir_folder(folder_path):
+    list_from_folder=[]
+    my_folder=os.listdir(folder_path)
+    for str_1 in my_folder:
+        list_from_folder.append(os.path.join(folder_path,str_1))
+    return list_from_folder
+def generate_a_list_of_folders(list_path):
+    list_folders=[]
+    for str_1 in list_path:
+        if os.path.isdir(str_1):
+            list_folders.append(str_1)
+    return list_folders
+
+def generate_a_list_of_files(list_path):
+    list_files=[]
+    for str_1 in list_path:
+        if os.path.isfile(str_1):
+            list_files.append(str_1)
+    return list_files
+my_path = "F:/Vadim/PycharmProjects/First"
+
+print(generate_a_list_of_files(listdir_folder(my_path)))
+print(generate_a_list_of_folders(listdir_folder(my_path)))
+
+
+
